3e318382f3ae1ec8a7f331719302d4e4eccef87d
문희연 <yeon7513@nate.com>
2026-03-20 10:05:35 +0900

    Initial commit

---
+++ src/main/java/com/ss_dam/SsDamApplication.java
@@ -0,0 +1,13 @@
+package com.ss_dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsDamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss_dam/SsDamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss_dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsDamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
