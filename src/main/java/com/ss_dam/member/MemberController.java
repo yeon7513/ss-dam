1968e0c8f85ea1f2f7ecff393a0f852b191fefe1
문희연 <yeon7513@nate.com>
2026-03-24 14:45:50 +0900

    회원 패키지 추가

---
+++ src/main/java/com/ss_dam/member/MemberController.java
@@ -0,0 +1,10 @@
+package com.ss_dam.member;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+}
