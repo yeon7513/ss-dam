07e4b5dc4bd94000361cfb2f1ea5f89c2dca50a0
ga0824ram-sys <ga0824ram@gmail.com>
2026-03-27 13:00:28 +0900

    실행 테스트
    스프링 부트와 리액트 연동 테스트

--- src/main/java/com/ss_dam/member/MemberController.java
+++ src/main/java/com/ss_dam/member/MemberController.java
@@ -1,12 +1,16 @@
 package com.ss_dam.member;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-  
 
+  @GetMapping("/test")
+  String test() {
+    return "test";
+  }
 
 }
