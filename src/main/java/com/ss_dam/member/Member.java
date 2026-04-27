2e701d023d3ff22bfa439fd09bd8eb95f9da6b61
bjimin0728-oss <bjimin0728@gmail.com>
2026-04-27 21:25:22 +0900

    Member dto 만들었다~

---
+++ src/main/java/com/ss_dam/member/Member.java
@@ -0,0 +1,95 @@
+package com.ss_dam.member;
+
+public class Member {
+private String id;
+private String password;
+private String name;
+private String phone;
+private String address;
+private String profileImg;
+private int rating;
+private int ranking;
+private int point;
+private String status;
+private String createdAt;
+private String updatedAt;
+private String loggedAt;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getProfileImg() {
+	return profileImg;
+}
+public void setProfileImg(String profileImg) {
+	this.profileImg = profileImg;
+}
+public int getRating() {
+	return rating;
+}
+public void setRating(int rating) {
+	this.rating = rating;
+}
+public int getRanking() {
+	return ranking;
+}
+public void setRanking(int ranking) {
+	this.ranking = ranking;
+}
+public int getPoint() {
+	return point;
+}
+public void setPoint(int point) {
+	this.point = point;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public String getCreatedAt() {
+	return createdAt;
+}
+public void setCreatedAt(String createdAt) {
+	this.createdAt = createdAt;
+}
+public String getUpdatedAt() {
+	return updatedAt;
+}
+public void setUpdatedAt(String updatedAt) {
+	this.updatedAt = updatedAt;
+}
+public String getLoggedAt() {
+	return loggedAt;
+}
+public void setLoggedAt(String loggedAt) {
+	this.loggedAt = loggedAt;
+}
+}
