dfb4d2d19a99babf00abfa8b0edeb19d5c7d843a
bjimin0728-oss <bjimin0728@gmail.com>
2026-04-28 13:59:27 +0900

    dto생성

---
+++ src/main/java/com/ss_dam/admin/adminAccount.java
@@ -0,0 +1,68 @@
+package com.ss_dam.admin;
+
+public class adminAccount {
+	private String empId;
+	private String password;
+	private String name;
+	private int phone;
+	private String dept;
+	private int role;
+	private String createdAt;
+	private String updatedAt;
+	private String loggedAt;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getLoggedAt() {
+		return loggedAt;
+	}
+	public void setLoggedAt(String loggedAt) {
+		this.loggedAt = loggedAt;
+	}
+	
+}
---
+++ src/main/java/com/ss_dam/challenge/Challenge.java
@@ -0,0 +1,67 @@
+package com.ss_dam.challenge;
+
+public class Challenge {
+	private int code;
+	private String empId;
+	private String title;
+	private String text;
+	private String startDate;
+	private String endDate;
+	private String status;
+	private String createdAt;
+	private String updateAt;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(String updateAt) {
+		this.updateAt = updateAt;
+	}
+}
