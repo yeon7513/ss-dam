5e935749a433bb83450f9d77fe40c0b9ef21377e
bjimin0728-oss <bjimin0728@gmail.com>
2026-05-14 09:51:22 +0900

    Update ChallengeGuide.jsx

--- frontend/src/pages/about/ChallengeGuide.jsx
+++ frontend/src/pages/about/ChallengeGuide.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const ChallengeGuide = () => {
-    return (
-        <div>
-            <h2>ChallengeGuide</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>ChallengeGuide</h2>
+    </div>
+  );
 };
 
 export default ChallengeGuide;
