013f81556ddcf4938558d3a052a94a0d906add53
bjimin0728-oss <bjimin0728@gmail.com>
2026-05-19 11:07:40 +0900

    challenge CRUD

--- src/main/java/com/ss_dam/challenge/controller/ChallengeController.java
+++ src/main/java/com/ss_dam/challenge/controller/ChallengeController.java
@@ -3,24 +3,49 @@ package com.ss_dam.challenge.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ss_dam.challenge.Challenge;
 import com.ss_dam.challenge.service.ChallengeService;
 
-
 @RestController
-@RequestMapping ("/challenge")
+@RequestMapping("/challenge")
 public class ChallengeController {
 
-	@Autowired
+    @Autowired
     ChallengeService challengeService;
-	
-	@GetMapping
-	public List<Challenge> getAllChallenges() {
-		
-	    return challengeService.findAll();
-	}
+
+    // 전체 조회
+    @GetMapping
+    public List<Challenge> getAllChallenges() {
+        return challengeService.findAll();
+    }
+
+    // 상세 조회
+    @GetMapping("/{code}")
+    public Challenge getChallenge(@PathVariable int code) {
+        return challengeService.findByCode(code);
+    }
+
+    // 등록
+    @PostMapping
+    public void add(@RequestBody Challenge item) {
+        challengeService.add(item);
+    }
+
+    // 수정
+    @PutMapping("/{code}")
+    public void update(@PathVariable int code,
+                       @RequestBody Challenge item) {
+
+        item.setCode(code);
+
+        challengeService.update(item);
+    }
+
+    // 삭제
+    @DeleteMapping("/{code}")
+    public void delete(@PathVariable int code) {
+        challengeService.delete(code);
+    }
 }
--- src/main/java/com/ss_dam/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/com/ss_dam/challenge/service/ChallengeServiceImpl.java
@@ -1,21 +1,41 @@
 package com.ss_dam.challenge.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.ss_dam.challenge.Challenge;
 import com.ss_dam.challenge.dao.ChallengeDao;
 
 @Service
 public class ChallengeServiceImpl implements ChallengeService {
 
-	@Autowired
-	ChallengeDao challengeDao;
-	
-	@Override
-	public List<Challenge> findAll() {
-	
-		return challengeDao.findAll();
-	}
+    @Autowired
+    ChallengeDao challengeDao;
 
+    @Override
+    public List<Challenge> findAll() {
+        return challengeDao.findAll();
+    }
+
+    @Override
+    public Challenge findByCode(int code) {
+        return challengeDao.findByCode(code);
+    }
+
+    @Override
+    public void add(Challenge item) {
+        challengeDao.add(item);
+    }
+
+    @Override
+    public void update(Challenge item) {
+        challengeDao.update(item);
+    }
+
+    @Override
+    public void delete(int code) {
+        challengeDao.delete(code);
+    }
 }
