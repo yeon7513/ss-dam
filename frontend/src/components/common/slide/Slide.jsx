25da8354d5590bf3878ff4aafc3b1d4ff1309998
문희연 <yeon7513@nate.com>
2026-05-19 16:31:13 +0900

    스와이퍼 슬라이드 추가

---
+++ frontend/src/components/common/slide/Slide.jsx
@@ -0,0 +1,23 @@
+import cn from 'classnames';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper } from 'swiper/react';
+import styles from './Slide.module.scss';
+
+// Slide 컴포넌트 사용법
+// <Slide> 안에 <SwiperSlide>를 import해서 사용하세요!!
+// import { SwiperSlide } from 'swiper/react';
+// <SwiperSlide>가 하나의 슬라이드 콘텐츠 입니다.
+
+function Slide({ children, className }) {
+  return (
+    <Swiper
+      pagination={true}
+      modules={[Pagination, Navigation]}
+      className={cn(styles.wrap, className)}
+    >
+      {children}
+    </Swiper>
+  );
+}
+
+export default Slide;
