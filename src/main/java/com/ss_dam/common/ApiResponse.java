1075bd2db03b52aa8ebadd5f188effe693c27bd7
GHun-J <jjh012743@gmail.com>
2026-08-21 09:53:20 +0900

    ApiResponse 주석

--- src/main/java/com/ss_dam/common/ApiResponse.java
+++ src/main/java/com/ss_dam/common/ApiResponse.java
@@ -1,9 +1,11 @@
 package com.ss_dam.common;
 
+
+// 필요할때마다 가져다 써서 프론트 엔드에 일관적인 형태(JSON)로 보내려고 만들어 놓은거
 public class ApiResponse<T> {
 	private boolean success;
-	private String message;
-	private T data;
+	private String message; //사용자한테 보내려는 응답 메세지
+	private T data; //T 데이터 타입은 그냥 들어오는 데이터 값대로 바뀌는 데이터 타입
 
 	public ApiResponse(boolean success, String message, T data) {
 		this.success = success;
