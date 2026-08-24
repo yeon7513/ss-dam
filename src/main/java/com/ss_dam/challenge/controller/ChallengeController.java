5b1b40beb2b57104bfc47032e4daa9cc1910eade
GHun-J <jjh012743@gmail.com>
2026-08-24 12:25:10 +0900

    ApiResponse 작업중

--- src/main/java/com/ss_dam/challenge/controller/ChallengeController.java
+++ src/main/java/com/ss_dam/challenge/controller/ChallengeController.java
@@ -5,6 +5,7 @@ import com.ss_dam.challenge.service.ChallengeService;
 import com.ss_dam.common.ApiResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,25 +19,25 @@ public class ChallengeController {
 
 	// 전체 챌린지 조회
 	@GetMapping
-	public ApiResponse<List<Challenge>> searchChallenges() {
+	public ResponseEntity<ApiResponse<List<Challenge>>> searchChallenges() {
 		List<Challenge> challenges = challengeService.searchChallenges();
-		
-		return ApiResponse.success("전체 챌린지 조회에 성공했습니다", challenges);
+
+		return ResponseEntity.ok(ApiResponse.success("전체 챌린지 조회에 성공했습니다", challenges));
 	}
 
 	// 챌린지 상세 조회
 	@GetMapping("/{code}")
-	public ApiResponse<Challenge> searchChallengeByCode(@PathVariable int code) {
+	public ResponseEntity<ApiResponse<Challenge>> searchChallengeByCode(@PathVariable int code) {
 		Challenge challenge = challengeService.searchChallengeByCode(code);
-		
-		return ApiResponse.success("챌린지 상세 조회에 성공했습니다", challenge);
+
+		return ResponseEntity.ok(ApiResponse.success("챌린지 상세 조회에 성공했습니다", challenge));
 	}
 
 	// 인기 챌린지 TOP 3
 	@GetMapping("/popular")
 	public ApiResponse<List<Challenge>> searchPopularChallenges() {
 		List<Challenge> challenges = challengeService.searchPopularChallenges();
-		
+
 		return ApiResponse.success("인기 챌린지 조회에 성공했습니다", challenges);
 	}
 
@@ -44,15 +45,15 @@ public class ChallengeController {
 	@GetMapping("/latest")
 	public ApiResponse<Challenge> searchLatestChallenge() {
 		Challenge challenge = challengeService.searchLatestChallenge();
-		
+
 		return ApiResponse.success("최신 등록 챌린지 조회에 성공했습니다", challenge);
 	}
 
 	// 챌린지 등록
 	@PostMapping
-	public ApiResponse<Void> registerChallenge(@RequestBody Challenge challenge) {		
+	public ApiResponse<Void> registerChallenge(@RequestBody Challenge challenge) {
 		challengeService.registerChallenge(challenge);
-		
+
 		return ApiResponse.success("챌린지가 성공적으로 등록되었습니다", null);
 	}
 
@@ -62,7 +63,7 @@ public class ChallengeController {
 
 		challenge.setCode(code);
 		challengeService.updateChallenge(challenge);
-		
+
 		return ApiResponse.success("챌린지가 성공적으로 수정되었습니다", null);
 	}
 
@@ -70,7 +71,7 @@ public class ChallengeController {
 	@DeleteMapping("/{code}")
 	public ApiResponse<Void> deleteChallenge(@PathVariable int code) {
 		challengeService.deleteChallenge(code);
-		
+
 		return ApiResponse.success("챌린지가 성공적으로 삭제되었습니다", null);
 	}
 }
