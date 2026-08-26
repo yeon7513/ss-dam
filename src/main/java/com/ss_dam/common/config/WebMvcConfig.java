4af6e70c89a299fff2c9e7ad5cd9fa8f433824b8
GHun-J <jjh012743@gmail.com>
2026-08-26 09:30:21 +0900

    Interceptor 끔

--- src/main/java/com/ss_dam/common/config/WebMvcConfig.java
+++ src/main/java/com/ss_dam/common/config/WebMvcConfig.java
@@ -1,17 +1,19 @@
 package com.ss_dam.common.config;
 
+// 인터셉터 잠시 꺼두려고 주석처리
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ss_dam.common.interceptor.AuthInterceptor;
 
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new AuthInterceptor())
-				.addPathPatterns("/api/member/**","/api/admin/**", "/member/**", "/admin/**");
-	}
-}
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//	
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(new AuthInterceptor())
+//				.addPathPatterns("/api/member/**","/api/admin/**", "/member/**", "/admin/**");
+//	}
+//}
