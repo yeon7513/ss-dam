f7e0f9780e6e2d9d6d1a8d1cc9cc2fd335e6a17b
GHun-J <jjh012743@gmail.com>
2026-09-10 18:16:13 +0900

    feed 좋아요 끝

--- src/main/java/com/ss_dam/common/likes/dao/LikeDaoImpl.java
+++ src/main/java/com/ss_dam/common/likes/dao/LikeDaoImpl.java
@@ -1,10 +1,11 @@
 package com.ss_dam.common.likes.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import com.ss_dam.common.likes.FeedLike;
 
 @Repository
 public class LikeDaoImpl implements LikeDao {
@@ -12,39 +13,51 @@ public class LikeDaoImpl implements LikeDao {
     @Autowired
     SqlSession sql;
 
-    @Override
-    public int countFeedLike(Long feedCode) {
+	@Override
+	public void upsertFeedLike(long feedCode, long memCode) {
+		Map<String, Object> params = new HashMap<>();
+		
+		params.put("feedCode", feedCode);
+		params.put("memCode", memCode);
+		
+		sql.insert("feedLike.upsertFeedLike", params);
+		
+	}
 
-        return sql.selectOne("likes.countFeedLike", feedCode);
-    }
+	@Override
+	public boolean selectIsFeedLiked(long feedCode, long memCode) {
+		Map<String, Object> params = new HashMap<>();
+		
+		params.put("feedCode", feedCode);
+		params.put("memCode", memCode);
+		
+		Boolean isLiked = sql.selectOne("feedLike.selectedIsLiked", params);
+		
+		return Boolean.TRUE.equals(isLiked);
+	}
 
-    @Override
-    public int countCommentLike(Long cmtCode) {
+	@Override
+	public void upsertCommentLike(long cmtCode, long memCode) {
+		Map<String, Object> params = new HashMap<>();
+		
+		params.put("cmtCode", cmtCode);
+		params.put("memCode", memCode);
+		
+		sql.insert("comment_like.upsertCommentLike", params);
+		
+	}
 
-        return sql.selectOne("likes.countCommentLike", cmtCode);
-    }
+	@Override
+	public boolean selectIsCommentLike(long cmtCode, long memCode) {
+		Map<String, Object> params = new HashMap<>();
+		
+		params.put("cmtCode", cmtCode);
+		params.put("memCode", memCode);
+		
+		Boolean isLiked = sql.selectOne("commentLike.selectedIsLiked", params);
+		
+		return Boolean.TRUE.equals(isLiked);		
+	}
 
-    @Override
-    public FeedLike searchFeedLike(FeedLike feedLike) {
-
-        return sql.selectOne("likes.searchFeedLike", feedLike);
-    }
-
-    @Override
-    public int registerFeedLike(FeedLike feedLike) {
-
-        return sql.insert("likes.registerFeedLike", feedLike);
-    }
-
-    @Override
-    public int updateFeedLike(FeedLike feedLike) {
-
-        return sql.update("likes.updateFeedLike", feedLike);
-    }
-
-    @Override
-    public int deleteFeedLike(FeedLike feedLike) {
-
-        return sql.update("likes.deleteFeedLike", feedLike);
-    }
+    
 }
