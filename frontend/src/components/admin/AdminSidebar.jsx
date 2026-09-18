696fc79504e74b76b743f48a5abdf66483876111
garam <ga0824ram@gmail.com>
2026-09-18 15:06:54 +0900

    어드민대시보드 사이드바 수정

--- frontend/src/components/admin/AdminSidebar.jsx
+++ frontend/src/components/admin/AdminSidebar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
-//import styles from "./Header.module.scss";
 import styles from "./AdminSidebar.module.scss";
 
 const AdminSidebar = () => {
@@ -18,26 +17,29 @@ const AdminSidebar = () => {
 
   return (
     <aside className={styles.sidebar}>
-      {/* 왼쪽 로고 영역 : '쓰담쓰담' 부분 */}
-      <div className="logoArea">
-        <h1 className={styles.logoText}>
-          <Link to="/">쓰담쓰담</Link>
-        </h1>
+      <div className={styles.sidebarInner}>
+        {/* 왼쪽 로고 영역 : '쓰담쓰담' 부분 */}
+        {/* 문자열클래스와 모듈클래스를 함께 부여함 */}
+        <div className={`logoArea ${styles.logoContainer}`}>
+          <h1 className={styles.logoText}>
+            <Link to="/">쓰담쓰담</Link>
+          </h1>
+        </div>
+        <nav className={styles.sidebarMenu}>
+          <ul>
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <Link
+                  to={item.path}
+                  className={location.pathname === item.path ? "active" : ""}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      <nav className={styles.sidebarMenu}>
-        <ul>
-          {menuItems.map((item) => (
-            <li key={item.path}>
-              <Link
-                to={item.path}
-                className={location.pathname === item.path ? "active" : ""}
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
     </aside>
   );
 };
